73cd553394f7660de72492ad4677330e0694fe0d
Romie Bett <bett@Betts-MacBook-Air.local>
2023-12-17 23:01:14 +0300

    adds a fix to cors errors

--- my-apollo-server.js
+++ my-apollo-server.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-micro');
+const microCors = require('micro-cors');
 const fetch = require('node-fetch');
 
-const app = express();
+const cors = microCors({
+  allowMethods: ['GET', 'POST', 'OPTIONS'],
+});
 
 const typeDefs = gql`
   type Query {
@@ -42,15 +44,9 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  const PORT = process.env.PORT || 4000;
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}${server.graphqlPath}`);
-  });
-}
-
-startServer();
+module.exports = cors((req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  return server.createHandler()(req, res);
+});
